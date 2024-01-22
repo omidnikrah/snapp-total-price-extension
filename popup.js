00ff18cd57744440c6d4f2698f4f8b1a00024c91
Omid Nikrah <omidnikrah@gmail.com>
2024-01-22 20:56:11 +0330

    fix: remove get storage

--- popup.js
+++ popup.js
@@ -31,58 +31,56 @@ document.addEventListener('DOMContentLoaded', async () => {
 			loadingElem.style.display = "block";
 			totalElem.style.display = "none";
 			descriptionElem.style.display = "none";
-			chrome.storage.local.get('user', ({ user }) => {
 
-				(async () => {
-					const token = `Bearer ${accessToken}`;
-					const headers = new Headers();
-					const url = 'https://web-api.snapp.ir/api/v1/ride/history';
-					const query = '?page=';
-					let total = 0;
-					let page = 1;
+			(async () => {
+				const token = `Bearer ${accessToken}`;
+				const headers = new Headers();
+				const url = 'https://web-api.snapp.ir/api/v1/ride/history';
+				const query = '?page=';
+				let total = 0;
+				let page = 1;
 
-					headers.append('Authorization', token);
-					headers.append('Content-Type', 'application/json');
+				headers.append('Authorization', token);
+				headers.append('Content-Type', 'application/json');
 
-					while (true) {
-						try {
-							const response = await fetch(`${url}${query}${page}`, {
-								method: 'GET',
-								headers
-							});
-							const data = await response.json();
+				while (true) {
+					try {
+						const response = await fetch(`${url}${query}${page}`, {
+							method: 'GET',
+							headers
+						});
+						const data = await response.json();
 
-							if (data.rides.length === 0) {
-								break;
-							}
-
-							total += data.rides
-								.filter(({
-									latest_ride_status
-								}) => latest_ride_status !== 6 && latest_ride_status !== 7)
-								.map(({
-									price
-								}) => price)
-								.reduce((sum, price) => sum + price, 0);
-							page += 1;
-						} catch (err) {
-							descriptionElem.innerText = `خیلی با اسنپ رفتی و الان بیشتر از ${page-1} ریکوئست نمیتونم بزنم اما اینقدر فعلا به اسنپ پول دادی :)`;
-							descriptionElem.style.margin = "-10px 20px 0";
-							descriptionElem.style.fontSize = "12px";
-							descriptionElem.style.display = "block";
+						if (data.rides.length === 0) {
 							break;
 						}
+
+						total += data.rides
+							.filter(({
+								latest_ride_status
+							}) => latest_ride_status !== 6 && latest_ride_status !== 7)
+							.map(({
+								price
+							}) => price)
+							.reduce((sum, price) => sum + price, 0);
+						page += 1;
+					} catch (err) {
+						descriptionElem.innerText = `خیلی با اسنپ رفتی و الان بیشتر از ${page-1} ریکوئست نمیتونم بزنم اما اینقدر فعلا به اسنپ پول دادی :)`;
+						descriptionElem.style.margin = "-10px 20px 0";
+						descriptionElem.style.fontSize = "12px";
+						descriptionElem.style.display = "block";
+						break;
 					}
-					calculateElem.disabled = false;
-					calculateElem.style.cursor = "pointer";
-					calculateElem.innerText = "دوباره حساب کن";
-					total = total / 10;
-					loadingElem.style.display = "none";
-					totalElem.style.display = "inline-block";
-					const numAnim = new CountUp("total", 0, total);
-					numAnim.start();
-				})();
-			});
+				}
+				calculateElem.disabled = false;
+				calculateElem.style.cursor = "pointer";
+				calculateElem.innerText = "دوباره حساب کن";
+				total = total / 10;
+				loadingElem.style.display = "none";
+				totalElem.style.display = "inline-block";
+				const numAnim = new CountUp("total", 0, total);
+				numAnim.start();
+			})();
 		})
 	} catch (err) {
 		descriptionElem.style.color = "#ff3434";
